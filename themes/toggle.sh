4ecb2e2d19ed79a774afc70cd492dc0daf8e9acf
feraxhp <feraxhp+gh@gmail.com>
2026-01-12 20:10:30 -0500

    :noctalia

--- themes/toggle.sh
+++ themes/toggle.sh
@@ -3,15 +3,20 @@
 theme_file="$HOME/.config/niri/theme.kdl"
 [[ -f "$theme_file" ]] || { echo "the theme file does not exist"; exit 1; }
 
-theme=$1 # 1 ligth 0 dark
-[[ -z "$theme" ]] && { echo "no theme selected: 1 ligth 0 dark"; exit 1; }
-
-[[ 0 -eq "$theme" ]] && { 
-    # dark
-    echo 'include "themes/mocha.kdl"' > "$theme_file"
+theme=$1 # 0 ligth 1 dark
+[[ -z "$theme" ]] && { 
+    echo "no theme selected: 1 ligth 0 dark"; 
+    exit 1; 
 }
 
-[[ 1 -eq "$theme" ]] && { 
+[[ "0" == "$theme" || "true" == "$theme" ]] && { 
+    # dark
+    echo 'include "themes/mocha.kdl"' > "$theme_file"
+    exit 0
+}
+
+[[ "1" == "$theme" || "false" == "$theme" ]] && { 
     # light
     echo 'include "themes/latte.kdl"' > "$theme_file"
+    exit 0
 }
