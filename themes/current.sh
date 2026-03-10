9169ddfcf4d82eae6165cea4f4014fbe50436185
feraxhp <feraxhp+gh@gmail.com>
2026-03-10 09:50:25 -0500

    ++ general improvements

--- themes/current.sh
+++ themes/current.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-config="$HOME/.config/noctalia/settings.json"
-current=$(jq ".colorSchemes.darkMode" < $config)
+# config="$HOME/.config/noctalia/settings.json"
+current=$(qs -c noctalia-shell ipc call state all | jq ".settings.colorSchemes.darkMode")
 "$HOME/.config/niri/themes/toggle.sh" "$current"
